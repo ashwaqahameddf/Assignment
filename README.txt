c5c9a07c42da828f4d14adf7fa0f2d6fda5ad5e9
ashwaqahameddf <ashwaqahameddf@gmail.com>
2014-09-16 19:00:34 -0700

    Assignment2

---
+++ README.txt
@@ -0,0 +1,18 @@
+Hi,
+Name-Ashwaq Ahamed DF
+USN-1PI12IS023
+
+my team mate sandeep J had done the both the parts himself and sent you... so talking to you, you said to omit the logging part for me.
+So, this assignment doesn't consist of the loggin section. 
+
+Question No- 05
+You are given a .csv file which has been written in the following format
+COUNTRY,CAPITAL,POPULATION
+Write a program which reads data from the .csv file and populates it into a .txt file in the form
+of sentences like:
+The capital of <COUNTRY> is <CAPITAL> with a population of <POPULATION>.
+
+My source code contains only one file,named as asn2_5.c and i have another data.csv file which contains data from which source code take inputs and stored in ash.c file.
+Instructions:
+compile: gcc asn2_5.c
+execute: ./a.out
---
+++ asn2_5.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(void)
+ {
+  int lineCount=0,p=0,x=0;
+  FILE *file;
+  FILE *fp;
+  fp=fopen("ash.c","w");
+  file=fopen("data.csv","r");
+   if(file==NULL)
+     {
+       printf("file is not opened properly\n");
+       return -1;
+     }
+  char line[100];  
+
+  while ((fgets(line,sizeof(line),file)) != NULL) 
+    {
+     lineCount++;
+    }
+  char *lineArray[lineCount-1];
+  char *token;
+  const char *j=",";
+  rewind(file);
+  char *country_name[lineCount];
+  char *capital_name[lineCount];
+  long int population[lineCount];
+  while ((fgets(line,sizeof(line),file)) != NULL) 
+   {
+      lineArray[p]=(char *)malloc(strlen(line));//1st bunch of memory allocated
+      strcpy(lineArray[p],line);
+      token = strtok(lineArray[p],j);
+      x=1;
+    
+      while( token != NULL) 
+        {
+            if((x%4)==3)
+              {
+                population[p] =atoi(token);
+                fprintf(fp," with a population of %ld.",population[p]);
+              }
+            else if((x%4)==1)
+              {
+                fprintf(fp, "The Capital of ");
+                country_name[p]=malloc(strlen(token));
+                strcpy(country_name[p],token);
+                fprintf(fp,"%s",country_name[p]);
+              }
+           else if((x%4)==2)
+              {
+                fprintf(fp," is ");
+                capital_name[p] = malloc(strlen(token));
+                strcpy(capital_name[p],token);
+                fprintf(fp,"%s",capital_name[p]);    
+              }
+           token = strtok(NULL, j);
+           x++;
+        }   //end of inner while
+    p++;
+    fprintf(fp,"\n");
+  }//end of outer while
+
+fprintf(fp,"\n");
+fclose(fp);
+fclose(file);
+return 0;
+}
